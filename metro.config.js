e7f69ce2bbd52d616046111c3ff836c0bd639e79
derrickdp <derrick@pattonhome.net>
2026-07-14 14:22:01 -0500

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const config = {
+  resolver: {
+    unstable_enablePackageExports: false,
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
