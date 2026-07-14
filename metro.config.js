f835aed9a49b026a878fc0b306f67510fd839968
derrickdp <derrick@pattonhome.net>
2026-07-14 16:24:47 -0500

    Update metro.config.js for Expo compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,15 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const config = {
-  resolver: {
-    unstable_enablePackageExports: false,
-  },
-};
+// const config = {
+//   resolver: {
+//     unstable_enablePackageExports: false,
+//   },
+// };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const { getDefaultConfig } = require('@expo/metro-config');
+const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
